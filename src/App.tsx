87ad9365af9b2e36e42eeac94cf93e41663e78c5
pgovostis <peter.govostis8@gmail.com>
2026-05-14 20:28:28 -0400

    Add Vercel Analytics integration

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
  */
 
 import { Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
@@ -39,6 +40,7 @@ export default function App() {
       </main>
       <Footer />
     </div>
+    <Analytics />
     </CartProvider>
   );
 }
